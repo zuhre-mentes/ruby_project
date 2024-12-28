1ff9b04d13d8cb8a4be441c72c0c74fed5c36f14
Doğan Gümüş <dogan.ggumus@gmail.com>
2024-12-28 04:53:33 +0300

    Add configuration and setup files for application environment
    - Added and updated scripts in the `bin` directory for application setup and execution.
    - Configured `config` files for database (database.yml), caching (cache.yml), and queuing (queue.yml).
    - Included environment-specific configurations (development.js, production.js, test.js).
    - Set up `devise` for authentication and localized strings (devise.en.yml).
    - Updated routing (routes.rb) and server configuration (puma.rb).
    - These changes provide the foundation for application setup, deployment, and runtime configuration.

--- config/routes.rb
+++ config/routes.rb
@@ -3,34 +3,54 @@ Rails.application.routes.draw do
   get "sessions/new"
   get "sessions/create"
   get "sessions/destroy"
+  devise_for :users
+  
+  resources :badges, only: [ :index, :show ]
+  resources :verifications, only: [ :new, :create, :show, :index ]
+  resources :forum_posts
 
-  # Sağlık durumu kontrolü
+  get "/verification_success", to: "verifications#success"
+  get "/verification_failure", to: "verifications#failure"
+
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # PWA rotaları
+  
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Kök rota - kayıt ekranı
+  
   root "sessions#new"
+  #root "badges#index" - Doğan 
 
-  # Kullanıcı rotaları (kayıt, düzenleme ve güncelleme dahil)
+  
   resources :users, only: [ :new, :create, :edit, :update ]
 
-  # Oturum (log in & log out) rotaları
+  
   resources :sessions, only: [ :new, :create, :destroy ]
   get "login", to: "sessions#new"
   delete "logout", to: "sessions#destroy", as: "logout"
   get "profile", to: "users#profile", as: "profile"
 
 
-  # Giriş sonrası ana sayfa
+  
   get "dashboard", to: "home#index"
 
   get "events/index", to: "events#index", as: "events_index"
 
   resources :posts, only: [:create]
   resources :posts, only: [:create, :destroy]
+  
+  resources :badges do
+    member do
+      get "related"
+    end
+  end
+
+  get "ai_chat/:id", to: "ai_chat#show", as: "ai_chat"
+
+  namespace :api do
+    resources :chat, only: [ :create ]
+  end
 
   
 end
